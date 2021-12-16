bcb1106d8aa8dc0dd993e14d3a9440b8b35b04c6
xatxat <xatma123@gmail.com>
2021-12-16 19:41:18 +0600

    Add new Ramp script, and new Ramps on track

---
+++ Assets/Scripts/RampScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RampScript : MonoBehaviour
+{
+    [SerializeField] private Renderer[] ramps;
+    [SerializeField] private Material ramp;
+
+    private void Awake()
+    {
+        foreach (Renderer rampRend in ramps)
+        {
+            rampRend.material = ramp;
+        }
+    }
+}
